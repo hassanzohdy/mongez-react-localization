ea7be90c1248f96fe52c87505a1c0b3c856aaea2
hassanzohdy <hassanzohdy@gmail.com>
2024-08-26 02:44:51 +0300

    "Released v3"

--- src/converters.tsx
+++ src/converters.tsx
@@ -2,30 +2,37 @@ import React from "react";
 
 /**
  * JSX converter
+ *
+ * @param translation The translation string containing placeholders.
+ * @param placeholders An object mapping placeholder names to their values.
+ * @param placeholderPattern A RegExp pattern for matching placeholders in the translation string.
+ * @returns An array of React elements where placeholders are replaced with their corresponding values.
  */
-export function jsxConverter(translation: string, placeholders: any) {
+export function jsxConverter(
+  translation: string,
+  placeholders: any,
+  placeholderPattern: RegExp,
+) {
   if (
     typeof placeholders !== "object" ||
     Object.keys(placeholders).length === 0
-  )
+  ) {
     return translation;
+  }
 
-  const expression = `(${Object.keys(placeholders)
-    .map(key => (key.startsWith(":") ? key : ":" + key))
-    .join("|")})`;
+  // Split the translation string based on the placeholder pattern
+  const parts = translation.split(placeholderPattern);
 
-  const regex = new RegExp(expression, "g");
+  // Map over the parts to replace placeholders with React.Fragment
+  return parts.map((part, index) => {
+    // Check if this part is a placeholder or text
+    if (index % 2 === 1) {
+      // This is a placeholder (odd index) based on splitting pattern
+      const placeholderKey = part; // Get the placeholder key
+      const value = placeholders[placeholderKey];
+      part = value === undefined || value === null ? placeholderKey : value;
+    }
 
-  return translation
-    .split(regex)
-    .filter(item => item !== undefined)
-    .map((part, index) => {
-      if (part.startsWith(":")) {
-        part = part.substring(1);
-      }
-
-      let value = placeholders[part] !== undefined ? placeholders[part] : part;
-
-      return <React.Fragment key={index}>{value}</React.Fragment>;
-    });
+    return <React.Fragment key={index}>{part}</React.Fragment>;
+  });
 }
